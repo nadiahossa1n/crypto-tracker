e12b1a492cdbef9e84853391cea509ed5fe9b71b
Nadia <nh894@nyu.edu>
2024-11-09 20:38:12 -0500

    header added

--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -1,9 +1,29 @@
+import TemporaryDrawer from "./drawer";
 import "./styles.css";
 
 function Header() {
   return (
-    <div className="header">
-      Header
+    <div className="navbar">
+      <h1 className="logo">
+        CryptoTracker<span style={{ color: "var(--blue)" }}>.</span>
+      </h1>
+      <div className="links">
+        <a href="/">
+          <p className="link">Home</p>
+        </a>
+        <a href="/">
+          <p className="link">Compare</p>
+        </a>
+        <a href="/">
+          <p className="link">Watchlist</p>
+        </a>
+        <a href="/">
+          <p className="link">Dashboard</p>
+        </a>
+      </div>
+      <div className="mobile-drawer">
+        <TemporaryDrawer />
+      </div>
     </div>
   )
 }
