9e806b003a271ff844c6e9dfff678228f7be5ea2
Nadia <nh894@nyu.edu>
2024-11-21 19:45:43 -0500

    coingecko api get request integrated

--- src/components/Dashboard/Tabs/index.js
+++ src/components/Dashboard/Tabs/index.js
@@ -1,7 +1,64 @@
 import React from "react";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-function TabsComponent() {
-  return <div>TabsComponent</div>;
+export default function TabsComponent({ coins }) {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+      },
+    },
+  });
+
+  const style = {
+    color: "var(--white)",
+    fontSize: "1.2rem",
+    fontWeight: 600,
+    fontFamily: "Inter",
+    textTransform: "capitalize",
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <TabContext value={value}>
+        <TabList onChange={handleChange} variant="fullWidth">
+          <Tab label="Grid" value="1" sx={style} />
+          <Tab label="List" value="2" sx={style} />
+        </TabList>
+        <TabPanel value="1">
+          <div>
+            {coins.map((item, i) => {
+              return (
+                <p key={i}>
+                  {i + 1}.{item.name}
+                </p>
+              );
+            })}
+          </div>
+          <div>{coins.name}</div>
+        </TabPanel>
+        <TabPanel value="2">
+          <div>
+            {coins.map((item, i) => {
+              return (
+                <p key={i}>
+                  {i + 1}.{item.id}
+                </p>
+              );
+            })}
+          </div>
+        </TabPanel>
+      </TabContext>
+    </ThemeProvider>
+  );
 }
-
-export default TabsComponent;
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Common/Header";
 import TabsComponent from "../components/Dashboard/Tabs";
+import axios from "axios";
 
 function DashboardPage() {
-  return <div>
-    <Header />
-    <TabsComponent />
-  </div>;
+  const [coins, setCoins] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "https://api.coingecko.com/api/v3/coins/markets",
+    params: { vs_currency: "usd" },
+    headers: {
+      accept: "application/json",
+      "x-cg-demo-api-key": "CG-MP5yijyBeggk3MU7Ygxuk5n9",
+    },
+  };
+
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((res) => setCoins(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <TabsComponent coins={coins} />
+    </div>
+  );
 }
 
 export default DashboardPage;
